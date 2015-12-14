c33ef69dac5c80c9b3bb150268dde2f275cfa845
Francisco Silva G. <fcosilva@yahoo.com>
2015-12-14 19:24:07 -0300

    habilitar el control de perfiles para controlador resultados #2

--- controllers/resultado.py
+++ controllers/resultado.py
@@ -2,49 +2,47 @@
 import StringIO
 import csv
 
-COLORES = """000033	000066	000099	0000CC	0000FF
-003300	003333	003366	003399	0033CC	0033FF
-006600	006633	006666	006699	0066CC	0066FF
-009900	009933	009966	009999	0099CC	0099FF
-00CC00	00CC33	00CC66	00CC99	00CCCC	00CCFF
-00FF00	00FF33	00FF66	00FF99	00FFCC	00FFFF
-330000	330033	330066	330099	3300CC	3300FF
-333300	333333	333366	333399	3333CC	3333FF
-336600	336633	336666	336699	3366CC	3366FF
-339900	339933	339966	339999	3399CC	3399FF
-33CC00	33CC33	33CC66	33CC99	33CCCC	33CCFF
-33FF00	33FF33	33FF66	33FF99	33FFCC	33FFFF
-660000	660033	660066	660099	6600CC	6600FF
-663300	663333	663366	663399	6633CC	6633FF
-666600	666633	666666	666699	6666CC	6666FF
-669900	669933	669966	669999	6699CC	6699FF
-66CC00	66CC33	66CC66	66CC99	66CCCC	66CCFF
-66FF00	66FF33	66FF66	66FF99	66FFCC	66FFFF
-990000	990033	990066	990099	9900CC	9900FF
-993300	993333	993366	993399	9933CC	9933FF
-996600	996633	996666	996699	9966CC	9966FF
-999900	999933	999966	999999	9999CC	9999FF
-99CC00	99CC33	99CC66	99CC99	99CCCC	99CCFF
-99FF00	99FF33	99FF66	99FF99	99FFCC	99FFFF
-CC0000	CC0033	CC0066	CC0099	CC00CC	CC00FF
-CC3300	CC3333	CC3366	CC3399	CC33CC	CC33FF
-CC6600	CC6633	CC6666	CC6699	CC66CC	CC66FF
-CC9900	CC9933	CC9966	CC9999	CC99CC	CC99FF
-CCCC00	CCCC33	CCCC66	CCCC99	CCCCCC	CCCCFF
-CCFF00	CCFF33	CCFF66	CCFF99	CCFFCC	CCFFFF
-FF0000	FF0033	FF0066	FF0099	FF00CC	FF00FF
-FF3300	FF3333	FF3366	FF3399	FF33CC	FF33FF
-FF6600	FF6633	FF6666	FF6699	FF66CC	FF66FF
-FF9900	FF9933	FF9966	FF9999	FF99CC	FF99FF
-FFCC00	FFCC33	FFCC66	FFCC99	FFCCCC	FFCCFF
-FFFF00	FFFF33	FFFF66	FFFF99	FFFFCC	FFFFFF
-""".split()
-
+@auth.requires_login()
 
 def index():
+    # registros para paginaciòn
+    reg_por_pagina = 100
+    
+    # preparo ubicaciones a elegir: [(id_ubicacion, descripcion)] 
+    # solo provincias (para listar departamentos dentro de ellas)
+    prov = msa.ubicaciones.with_alias("provincia")
+    q = prov.clase=="Provincia"
+    ubicaciones = msa(q).select(prov.id_ubicacion, prov.descripcion )
+    ubicaciones = sorted([(row.id_ubicacion, row.descripcion) for row in ubicaciones],
+                         key= lambda x: x[1])
+
+    form = SQLFORM.factory(
+	Field("id_provincia", "string",
+	    label="Provincia",requires=IS_IN_SET(ubicaciones),
+	    default=session.id_provincia or None, ),
+        )
+           
+    if form.accepts(request.vars, session, keepvalues=True):
+        # formulario completado correctamente
+        # grabo en la sesión los datos del filtro (para paginación)
+        session.id_provincia = form.vars.id_provincia.strip() 
+##	pass
+	redirect(URL(f="filtro2"))
+    else:
+        # formulario mal completado o primera vez
+        ok = False
+        
+    return dict(form=form)
+
+def filtro2():
     "Página inical de búsqueda"
     # preparo ubicaciones a elegir: [(id_ubicacion, descripcion)]
-    ubicaciones = msa(msa.ubicaciones.clase.contains(["Pais", "Departamento", "Provincia"])).select()
+    q = msa.ubicaciones.clase.contains(["Pais", "Departamento", "Provincia"])
+    if session.id_provincia:
+    	q &= ubicaciones.id_ubicacion_padre == session.id_ubicacion
+
+    ubicaciones = msa(q).select()
+    
     ubicaciones = sorted([(row.id_ubicacion, "%s (%s)" % (row.descripcion, row.clase)) 
                           for row in ubicaciones] + [(None, "")], 
                          key=lambda x: (x[0]*1000 if x[0] and x[0] < 1000 else x[0]))
@@ -73,6 +71,7 @@ def index():
 
     return {'form': form}
     
+@auth.requires_membership(role="veedores")
 def reporte():
     "Página de resultados: datos totalizados por ubicación y cargo"
     # obtengo las variables del requerimiento (URL)
@@ -205,29 +204,12 @@ def reporte():
     # generar el chart
     listas = []
     porcentajes= []
-    colores = []
-    colorind = 0
-  
+
     for lista in tabla_resultado:
         listas.append(lista['nro_lista'])
         porcentajes.append(lista['porc'][:-2])
-        if lista['nro_lista'].strip() == '131':
-           colores.append("50EBEC")
-        elif lista['nro_lista'].strip() == '132':
-           colores.append("7200BF")
-        elif lista['nro_lista'].strip() == '133':
-           colores.append("7200BF")
-        elif lista['nro_lista'].strip() == '135':
-           colores.append("FEFF00")
-        elif lista['nro_lista'].strip() == '137':
-           colores.append("FF0000")
-	else:
-           if colorind >= len(COLORES):
-              colorind = 0
-           colores.append(COLORES[colorind])
-           colorind += 1
      
-    url = "http://chart.apis.google.com/chart?cht=p&chd=t:"+','.join(porcentajes)+"&chs=500x400&chl="+'|'.join(listas)+"&chco="+'|'.join(colores)+"&.png"
+    url = "http://chart.apis.google.com/chart?cht=p&chd=t:"+','.join(porcentajes)+"&chs=500x400&chl="+'|'.join(listas)+"&.png"
     chart = IMG(_src=url,_width="500",_height="400")
 
     # devuelvo los datos a la vista
@@ -350,56 +332,3 @@ def exportar():
 
     response.headers['Content-Type'] = 'text/csv'
     return s.getvalue()
-
-def pdfreport():
-    #response.title = "web2py sample report"
-    
-    # include a google chart!
-    url = "http://chart.apis.google.com/chart?cht=p3&chd=t:60,40&chs=250x100&chl=Hello|World&.png"
-    chart = IMG(_src=url, _width="250",_height="100")
-
-    # create a small table with some data:
-    data=reporte()
-    items=sorted(data['tabla_resultado'])
-    pdfchart = data['chart']
-    rows = [THEAD(TR(TH("Lista",_width="10%"), 
-                     TH(" ",_width="60%"), 
-                     TH("Votos",_width="10%"), 
-                     TH("Porc.",_width="10%"), 
-                     TH("Bancas",_width="10%"),_bgcolor="#A0A0A0")),
-            TBODY(*[TR(TD(item["nro_lista"]),
-                       TD(item["desc_lista"]),
-                       TD(item["votos"]),TD(item["porc"]),
-                       TD(item["bancas_obtenidas"]) ) 
-                    
-                    for item in items])]
-    
-    table = TABLE(*rows, _border="0", _width="100%", _class="table table-striped")
-    if not request.extension=="pdf":
-        from gluon.contrib.pyfpdf import FPDF, HTMLMixin
-
-        # create a custom class with the required functionalities 
-        class MyFPDF(FPDF, HTMLMixin):
-            def header(self): 
-                "hook to draw custom page header"
-                #logo=os.path.join(request.env.web2py_path,"applications",request.application,"private","tutorial","logo_pb.png")
-                #self.image(logo,10,8,33)
-                self.set_font('Arial','B',15)
-                self.cell(65) # padding
-                self.cell(60,10,response.title,1,0,'C')
-                #self.ln(20)
-                
-            def footer(self):
-                "hook to draw custom page header (printing page numbers)"
-                self.set_y(-15)
-                self.set_font('Arial','I',8)
-                txt = 'Page %s of %s' % (self.page_no(), self.alias_nb_pages())
-                self.cell(0,10,txt,0,0,'C')             
-        pdf=MyFPDF()
-        # create a page and serialize/render HTML objects
-        pdf.add_page()
-        pdf.write_html(str(XML(table, sanitize=False)))
-        pdf.write_html(str(XML(CENTER(pdfchart), sanitize=False)))
-        # prepare PDF to download:
-        response.headers['Content-Type']='application/pdf'
-        return pdf.output(dest='S')
